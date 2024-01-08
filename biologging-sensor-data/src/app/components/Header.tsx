48b3eee6a7ea0a6a72f81a49d870881ba4182f7d
Yuliia Mustafina <71671138+Mustafina1990@users.noreply.github.com>
2024-01-08 15:52:14 +0100

    update Header component styling and add search bar

--- biologging-sensor-data/src/app/components/Header.tsx
+++ biologging-sensor-data/src/app/components/Header.tsx
@@ -1,18 +1,19 @@
 import {default as logo} from "../assets/sbdi-logo-orginal-large.png";
+import "../index.css";
 
 const links = ["Data", "More details", "About"];
 
 export default function Header() {
   return (
-    <nav className="header flex justify-between items-center h-20 p-4">
+    <nav className="header">
       <div>
         <a href="./Header">
-          <img src={logo.src} alt="Swedish Biodiversity Data Infrastructure" className="h-10"/>
+          <img src={logo.src} alt="Swedish Biodiversity Data Infrastructure" className="logo"/>
         </a>
       </div>
-      <h1 className="ml-4 font-semibold mr-auto">Swedish Biodiversity Data Infrastructure</h1>
-      <input type="search" name="search-bar" id="" />
-      <ul className="flex gap-6 list-none font-semibold">
+      <h1 className="logo-name">Swedish Biodiversity Data Infrastructure</h1>
+      <input type="search" name="search-bar" id="" className="search-bar" />
+      <ul className="pages">
         {links.map((link) => <li key={link}><a href="#">{link}</a></li>)}
       </ul>
     </nav>
