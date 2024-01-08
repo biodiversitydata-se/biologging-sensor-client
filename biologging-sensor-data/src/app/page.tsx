681fe22d019a2f0feb49764254a1190852e08970
Yuliia Mustafina <71671138+Mustafina1990@users.noreply.github.com>
2024-01-08 16:06:37 +0100

    Add Detail component to Home page

--- biologging-sensor-data/src/app/page.tsx
+++ biologging-sensor-data/src/app/page.tsx
@@ -1,7 +1,14 @@
+'use client'
 import Image from "next/image";
+import Detail from "./pages/Detail";
+import Overview from './pages/Overview'
+import './index.css'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <Detail/>
+    </main>
   )
 }
