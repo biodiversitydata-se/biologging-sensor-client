1cc899f2ba268f72c269c223eb388c99acfc86ce
Zuzana <zuzana.roh@gmail.com>
2024-01-26 17:03:41 +0100

    INT: removed files and restructured

--- biologging-sensor-data/src/app/layout.tsx
+++ biologging-sensor-data/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
