068af83fb458933746d3278ff1288fc7f012cfdb
Zuzana <zuzana.roh@gmail.com>
2024-01-26 16:28:53 +0100

    Revert "Merge pull request #4 from biodiversitydata-se/detail-styling"
    This reverts commit 82f6834c6f8c79152128e296a0549be157e2b98b, reversing
    changes made to d670cb0ebfee1468f168390dcdbfdb14bfea3173.

--- biologging-sensor-data/src/app/visual/page.tsx
+++ biologging-sensor-data/src/app/visual/page.tsx
@@ -1,7 +1,26 @@
-export default function Visualize() {
-    return (
-        <div>
-            Todo
-        </div>
-    )
+'use client'
+import Overview from './VisualizationPage'
+// import './index.css'
+
+export default function Home() {
+  return (
+    <>
+    <main>
+      <Overview/>
+    </main>
+    </>
+  )
 }
+
+
+
+
+
+
+// export default function Visualize() {
+//     return (
+//         <div>
+//             Todo
+//         </div>
+//     )
+// }
--- biologging-sensor-data/src/components/overview/Snippet.tsx
+++ biologging-sensor-data/src/components/overview/Snippet.tsx
@@ -1,17 +1,29 @@
 import { Dataset } from "@/interfaces/dataset";
 import Link from "next/link";
 
+// #f2f2f2 - table header
+// #fafafa table content
+
 export default function OverviewSnippet({data}: {data: Dataset|null}) {
+    const bold = {
+        fontWeight: "bold",
+    }
     return(
         <div>
             {/* HEADER */}
-            <div>
+            <div style={{backgroundColor: "#f2f2f2", display: "flex", flexDirection:"row", justifyContent: "space-between", paddingLeft: "15px", paddingRight: "15px"}}>
                 <div>
-                    Dataset record:
+                    Dataset record: <span style={bold}>{data?.datasetID}</span>
                 </div>
 
                 <div>
-                    <Link href="/detail"> Show all information about this dataset</Link>                    
+                    <Link href={{
+                          pathname: `/detail/[id]`,
+                          query: {
+                            id: data?.datasetID,
+                          },
+                        }}
+                        as={`/detail/${data?.datasetID}`}> Show all information about this dataset</Link>                    
                 </div>
 
                 <div>
@@ -21,24 +33,24 @@ export default function OverviewSnippet({data}: {data: Dataset|null}) {
             </div>
 
             {/* CONTENT */}
-            <div>
-                <div>
-                    <div>Title:</div>
+            <div style={{backgroundColor: "#fafafa", paddingLeft: "15px", paddingRight: "15px"}}>
+                <div className="row">
+                    <div className="col-md-2" style={bold}>Title:</div>
                     <div>{data?.datasetTitle}</div>
                 </div>
 
-                <div>
-                    <div>Description:</div>
+                <div className="row">
+                    <div className="col-md-2" style={bold}>Description:</div>
                     <div>{data?.datasetDescription}</div>
                 </div>
 
-                <div>
-                    <div>Instruments:</div>
+                <div className="row">
+                    <div className="col-md-2" style={bold}>Instruments:</div>
                     <div>{data?.instrumentTypes?.join(", ")}</div>
                 </div>
 
-                <div>
-                    <div>Sensors:</div>
+                <div className="row">
+                    <div className="col-md-2" style={bold}>Sensors:</div>
                     <div>{data?.sensorTypes?.join(", ")}</div>
                 </div>                
             </div>
