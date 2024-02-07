0063115ce776f73ee37257bbefb3a3f0c16610f7
Zuzana <zuzana.roh@gmail.com>
2024-02-07 18:02:47 +0100

    BIOSENSOR-21: installed and setup leafletjs

--- biologging-sensor-data/src/components/graphs/map/MapGraph.tsx
+++ biologging-sensor-data/src/components/graphs/map/MapGraph.tsx
@@ -1,7 +1,20 @@
+import './MapGraph.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 export default function MapGraph() {
     return (
-        <div>
-            TODO: MAP GRAPH
+        <div style={{ height: "300px", }}>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "300px" }}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
