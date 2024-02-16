7db90a2105315205be52fe191005d4f3ad605830
Yuliia Mustafina <71671138+Mustafina1990@users.noreply.github.com>
2024-02-16 01:51:07 +0100

    add dataset selection functionality to LineGraph component

--- biologging-sensor-data/src/components/graphs/line/LineGraph.tsx
+++ biologging-sensor-data/src/components/graphs/line/LineGraph.tsx
@@ -16,11 +16,15 @@ import { filterRecords } from '@/api/record/api';
 import { Record } from '@/api/record/record.interface';
 
 export default function LineGraph({ events }: { events: Event[] }) {
+  const [selectedDataset, setSelectedDataset] = useState("");
   const [options, setOptions] = useState<ChartOptions>({
     responsive: true,
     plugins: {
       legend: {
         position: 'top' as const,
+        onClick: (evt, legendItem) => {
+          setSelectedDataset(legendItem.text);
+        },
       },
       title: {
         display: true,
@@ -47,7 +51,10 @@ export default function LineGraph({ events }: { events: Event[] }) {
   
   useEffect(() => {
     const dataFetch = async () => {
-      let newDatasets = [...datasets];
+      let newLabels = [];
+      let newAltitudes = [];
+      let newTemperatures = [];
+      let newPressures = [];
 
       for (let i = 0; i < events.length; i++) {
         const ids = [events[i].eventID];
@@ -55,12 +62,7 @@ export default function LineGraph({ events }: { events: Event[] }) {
         const result = await filterRecords({ eventIds: ids, datasetIds: ids2 });
         const records: Record[] = result.results;
 
-        const newLabels = [...labels];
-        const newAltitudes = [];
-        const newTemperatures = [];
-        const newPressures = [];
-
-        records.slice(0, 30).map(itm => {
+        records.slice(0, 30).filter(itm => itm.recordValues.altitude || itm.recordValues.temperature || itm.recordValues.pressure).map(itm => {
           if(!(newLabels.includes(itm.recordStart))) {
             let date = new Date(itm.recordStart);
             let hours = String(date.getUTCHours()).padStart(2, '0');
@@ -72,37 +74,40 @@ export default function LineGraph({ events }: { events: Event[] }) {
           newTemperatures.push(itm.recordValues.temperature);
           newPressures.push(itm.recordValues.pressure);
         });
-
-        newDatasets.push(
-          {
-            label: `Altitude`,
-            data: newAltitudes,
-            borderColor: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-            fill: false,
-          },
-          {
-            label: `Temperature`,
-            data: newTemperatures,
-            borderColor: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-            fill: false,
-          },
-          {
-            label: `Pressure`,
-            data: newPressures,
-            borderColor: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-            fill: false,
-          }
-        );
-
-        setLabels(newLabels);
-        setDatasets(newDatasets);
       }
+
+      const newDatasets = [
+        {
+          label: `Altitude`,
+          data: newAltitudes,
+          borderColor: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          fill: false,
+        },
+        {
+          label: `Temperature`,
+          data: newTemperatures,
+          borderColor: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          fill: false,
+        },
+        {
+          label: `Pressure`,
+          data: newPressures,
+          borderColor: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          fill: false,
+        }
+      ];
+
+      // Filter datasets based on selectedDataset
+      const filteredDatasets = newDatasets.filter(dataset => selectedDataset === "" || dataset.label === selectedDataset);
+
+      setLabels(newLabels);
+      setDatasets(filteredDatasets);
     };
     dataFetch();
-  }, [events]);
+  }, [events, selectedDataset]);
 
   const data = {
     labels,
