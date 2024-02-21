f68dbd02a425ec0001b886ea0e18b08c8ee16a79
Zuzana <zuzana.roh@gmail.com>
2024-02-21 12:10:37 +0100

    BIOSENSOR-58: changed prefix for css

--- biologging-sensor-data/next.config.js
+++ biologging-sensor-data/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'PROD'
+
 const nextConfig = {
     reactStrictMode: false,
+    assetPrefix: isProd ? 'http://canmove-dev.ekol.lu.se/biologging-client-dev/' : undefined,
 }
 
 module.exports = nextConfig
