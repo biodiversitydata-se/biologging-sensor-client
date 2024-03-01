867d71b000b891d88244139b8b99990e91de7796
Zuzana <zuzana.roh@gmail.com>
2024-03-01 16:55:33 +0100

    78: fixed fetching assets prefix based on env

--- biologging-sensor-data/next.config.js
+++ biologging-sensor-data/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'PROD'
+const isTest = process.env.NEXT_PUBLIC_NODE_ENV === 'test'
 
 const nextConfig = {
     reactStrictMode: false,
-    assetPrefix: isProd ? 'http://canmove-dev.ekol.lu.se/biologging-client-dev/' : undefined,
+    assetPrefix: isTest ? 'http://canmove-dev.ekol.lu.se/biologging-client-dev/' : undefined,
 }
 
 module.exports = nextConfig
