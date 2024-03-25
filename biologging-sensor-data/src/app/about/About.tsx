ec21bd8fb279459a9dcbb18b65867348a5f46546
Yuliia Mustafina <71671138+Mustafina1990@users.noreply.github.com>
2024-03-25 15:45:37 +0100

    add margin bottom to elements on About page

--- biologging-sensor-data/src/app/about/About.tsx
+++ biologging-sensor-data/src/app/about/About.tsx
@@ -3,14 +3,14 @@ export default function About() {
         <section className="home about">
             <div className="container">
                 <div className="col-md-offset-1 col-md-10">
-                    <div className="row">
+                    <div className="row" style={{marginBottom: '20px'}}>
                        <h2>About page</h2>
                        <p>SBDI Biologging tools manages data from animal sensor systems.  Biologging research involves the collection of time-series of sensor data, used to e.g. create migration tracks or foraging and homerange trajectories (e.g. tracking animals using GPS receivers)  and/or to characterize behaviour or physiology of animals. Main data providers to SBDI biologging are currently Lund University (the CAnMove animal movement database) and Swedish University of Life Sciences (the WRAM wireless remote animal monitoring database).</p>
                        <p>SBDI Biologging is part of the SBDI infrastructure.
 If you would like to connect through or API follow this link. An example use can be studied in the client repo <a href="https://github.com/biodiversitydata-se/biologging-sensor-client">https://github.com/biodiversitydata-se/biologging-sensor-client</a></p>
                     </div>
 
-                    <div className="row">
+                    <div className="row" style={{marginBottom: '20px'}}>
                        <h3>About the API</h3>
                        <p className="mt-3">All the data displayed on this website is obtained via the Biologging Open API, available here <a href="http://canmove-dev.ekol.lu.se:8080/biologgingAPI/v1/docs/#/">http://canmove-dev.ekol.lu.se:8080/biologgingAPI/v1/docs</a></p>
                        <p>Its datamodel is following <a href="https://github.com/biodiversitydata-se/biologging-sensor-datamodel">the format described here.</a></p>
