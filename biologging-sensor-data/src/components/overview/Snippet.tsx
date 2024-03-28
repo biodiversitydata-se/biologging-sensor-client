84f357bca63d4f8cd886f18d400b4b328475690f
Zuzana <zuzana.roh@gmail.com>
2024-03-28 18:01:01 +0100

    87: added icons next to the description

--- biologging-sensor-data/src/components/overview/Snippet.tsx
+++ biologging-sensor-data/src/components/overview/Snippet.tsx
@@ -1,6 +1,8 @@
 import { Dataset } from "@/api/dataset/dataset.interface";
 import { DetailLink, VisualisationLink } from "../links";
 import './Snippet.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartLine, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function OverviewSnippet({ data }: { data: Dataset | null }) {
@@ -24,22 +26,36 @@ export default function OverviewSnippet({ data }: { data: Dataset | null }) {
             <div style={{ backgroundColor: "#fafafa", paddingLeft: "15px", paddingRight: "15px" }}>
                 <div className="row">
                     <div className="col-md-2 bold">Title:</div>
-                    <div className="col-md-10">{data?.datasetTitle}</div>
+                    <div className="col-md-9">{data?.datasetTitle}</div>
                 </div>
 
                 <div className="row">
                     <div className="col-md-2 bold">Description:</div>
-                    <div className="col-md-10">{data?.datasetDescription}</div>
+                    <div className="col-md-8">{data?.datasetDescription}</div>
+                    <div className="col-md-2">
+                        <div className="row" style={{ display: "flex", justifyContent: "space-between" }}>
+                            <DetailLink datasetId={data?.datasetID}>
+                                <FontAwesomeIcon icon={faCircleInfo} className="snippet-icon" style={{ marginRight: "20px" }} />
+                            </DetailLink>
+
+                            {data?.numberOfRecords && data?.numberOfRecords > 0 && (
+                                <VisualisationLink datasetId={data?.datasetID}>
+                                    <FontAwesomeIcon icon={faChartLine} className="snippet-icon" />
+                                </VisualisationLink>
+                            )}
+
+                        </div>
+                    </div>
                 </div>
 
                 <div className="row">
                     <div className="col-md-2 bold">Instruments:</div>
-                    <div className="col-md-10">{data?.instrumentTypes?.join(", ")}</div>
+                    <div className="col-md-9">{data?.instrumentTypes?.join(", ")}</div>
                 </div>
 
                 <div className="row">
                     <div className="col-md-2 bold">Sensors:</div>
-                    <div className="col-md-10">{data?.valuesMeasured?.join(", ")}</div>
+                    <div className="col-md-9">{data?.valuesMeasured?.join(", ")}</div>
                 </div>
             </div>
         </div>
