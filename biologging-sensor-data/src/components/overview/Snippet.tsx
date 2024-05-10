ca00f89bd4fa6b2eef277fc867f2b847e5d467b7
Khosiyat <hosiyatsabirova@gmail.com>
2024-05-10 18:01:14 +0200

    **version changed to versions & **checking if versions exists and is a string before attempting to replace the dot with an underscore.

--- biologging-sensor-data/src/components/overview/Snippet.tsx
+++ biologging-sensor-data/src/components/overview/Snippet.tsx
@@ -6,13 +6,15 @@ import { faChartLine, faCircleInfo, faDownload } from "@fortawesome/free-solid-s
 
 export default function OverviewSnippet({ data }: { data: Dataset | null }) {
 
-    const downloadDataset = (datasetId: string, version: string) => {
+    const downloadDataset = (datasetId: string, versions?: string) => {
         const baseUrl = "http://canmove-dev.ekol.lu.se/biologgingPublicArchives/";
-        const formattedVersion = version?.replace(".", "_") || "1_0";
+        const formattedVersion = versions && typeof versions === 'string' ? versions.replace(".", "_") : "1_1";
         const downloadUrl = `${baseUrl}${datasetId}/${datasetId}_json_${formattedVersion}.zip`;
         window.open(downloadUrl);
     };
+
     const firstVersion = data?.versions && data.versions[0];
+
 
     return (
         <div>
@@ -53,7 +55,8 @@ export default function OverviewSnippet({ data }: { data: Dataset | null }) {
 
                                 {data?.numberOfRecords && data?.numberOfRecords > 0 ? (
                                  <div style={{ marginLeft: "10px" }}>
-                                    <FontAwesomeIcon icon={faDownload} className="snippet-icon" onClick={() => downloadDataset(data?.datasetID || '')} size="3x" style={{ color: "#1E4B75" }} />
+                                    <FontAwesomeIcon icon={faDownload} className="snippet-icon" onClick={() => downloadDataset(data?.datasetID || '', firstVersion)} size="3x" style={{ color: "#1E4B75" }} />
+
                                 </div>
                             ) : null}
 
