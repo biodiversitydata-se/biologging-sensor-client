2ed992f1cd1a634d13c851bbd0e014ef82a53db3
Yuliia Mustafina <71671138+Mustafina1990@users.noreply.github.com>
2024-05-18 20:04:52 +0200

    refactor: Improve email copy functionality in Detail.tsx

--- biologging-sensor-data/src/app/detail/[id]/Detail.tsx
+++ biologging-sensor-data/src/app/detail/[id]/Detail.tsx
@@ -157,7 +157,7 @@ function Detail({ detail }: { detail: Dataset | null }) {
                                         </div>
                                     ) : null}
                                     
-                                    <span className='col-md-5'>{person.email ? person.email : null}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-copy"  style={{ marginLeft: '15px' }} viewBox="0 0 16 16">
+                                    <span className='col-md-5'>{person.email ? person.email : null}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-copy"  style={{ marginLeft: '15px', cursor: 'pointer' }} viewBox="0 0 16 16" onClick={() => { copy(person.email); setCopyMessage('Email copied!'); setIsCopied(true); }}>
                                         <path fillRule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
                                     </svg></span>
                                 </span>
