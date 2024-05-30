40e419dba7610b6b4e96d7d7635152db59bb3e7b
Zuzana <zuzana.roh@gmail.com>
2024-05-30 19:52:24 +0200

    98: fix default graph display according to config fil

--- biologging-sensor-data/src/app/visualisation/[id]/Visualisation.tsx
+++ biologging-sensor-data/src/app/visualisation/[id]/Visualisation.tsx
@@ -2,11 +2,20 @@ import { Event } from "@/api/event/event.typscript";
 import Actogram from "@/components/graphs/actogram/Actogram";
 import LineGraph from "@/components/graphs/line/LineGraph";
 import MapGraph from "@/components/graphs/map/MapGraph";
-import { sensorTypes } from "@/config/config";
+import { datasetConfig, sensorTypes } from "@/config/config";
+import { useEffect, useState } from "react";
 
 export default function Visualisation({ events, sensors }: { events: Event[], sensors: string[] }) {
     const SensorTypeDisplay = ({ sensor }: { sensor: string }) => {
-        const graphType = sensorTypes[sensor]?.graphType;
+        let graphType;
+
+        const dId = events[0].datasetID;
+        const customGraphs = datasetConfig[dId]?.customGraphs;
+        if (customGraphs && sensor in customGraphs) {
+            graphType = customGraphs[sensor].graphType;
+        } else {
+            graphType = sensorTypes[sensor].graphType;
+        }
 
         if (!graphType) return <div>No visualisation</div>;
 
