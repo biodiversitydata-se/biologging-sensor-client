36c88278f962900a058bfe59443254788e169c93
Mathieu Blanchet <mathieu.blanchet@biol.lu.se>
2024-09-06 12:03:43 +0200

    add prod url api

--- biologging-sensor-data/src/config/constants.ts
+++ biologging-sensor-data/src/config/constants.ts
@@ -1,6 +1,7 @@
 // API 
 export const TEST_URL_BASE = '/biologging-client-dev/';
-export const BASE_URL = 'http://canmove-dev.ekol.lu.se:8080/biologgingAPI/v1/';
+//export const BASE_URL = 'http://canmove-dev.ekol.lu.se:8080/biologgingAPI/v1/';
+export const BASE_URL = 'http://biologging.biodiversitydata.se:8080/biologgingAPI/v1/docs/';
 export const DATASETS_ENDPOINT = 'datasets';
 export const EVENTS_ENPOINT = 'events';
 export const INSTRUMENT_ENPOINT = 'instruments';
